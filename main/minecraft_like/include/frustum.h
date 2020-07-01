01a9791ebb827f15804cc5344b08dfda87b64d1f
GJeannin0 <j6.guillaume@gmail.com>
2020-07-01 10:02:45 +0200

    frustum construct

--- main/minecraft_like/include/frustum.h
+++ main/minecraft_like/include/frustum.h
@@ -27,7 +27,7 @@ public:
 	Frustum(const Frustum&);
 	~Frustum();
 
-	void ConstructFrustum();
+	void ConstructFrustum(neko::Vec3f position, neko::Vec3f direction, float nearPlaneDistance, float farPlaneDistance, neko::degree_t fovx, neko::degree_t fovy, neko::Vec3f up, neko::Vec3f right);
 
 	bool ContainsPoint(neko::Vec3f point);
 	bool ContainsCube(neko::Aabb3d aabb);
--- main/minecraft_like/src/frustum.cpp
+++ main/minecraft_like/src/frustum.cpp
@@ -13,9 +13,34 @@ neko::Vec3f Plane::GetNormal(neko::Vec3f pointA, neko::Vec3f pointB, neko::Vec3f
 	return pointA.Cross(vecA, vecB).Normalized();
 }
 
-void Frustum::ConstructFrustum()
+void Frustum::ConstructFrustum(neko::Vec3f position, neko::Vec3f direction, float nearPlaneDistance, float farPlaneDistance, neko::degree_t fovx, neko::degree_t fovy, neko::Vec3f up, neko::Vec3f right) //TODO clean and optimize
 {
+	Plane nearP = Plane(position + direction.Normalized() * nearPlaneDistance, direction.Normalized());
+	Plane farP = Plane(position + direction.Normalized() * farPlaneDistance, direction.Normalized());
 
+	float heightNear = neko::Cos(fovx / 2) * nearPlaneDistance;
+	float widthNear = neko::Cos(fovy / 2) * nearPlaneDistance;
+	float heightFar = neko::Cos(fovx / 2) * farPlaneDistance;
+	float widthFar = neko::Cos(fovy / 2) * farPlaneDistance;
+
+	neko::Vec3f ntr = nearP._point + up * heightNear/2 + right * widthNear/2; //Near Top Right
+	neko::Vec3f nbr = nearP._point - up * heightNear/2 + right * widthNear/2;
+	neko::Vec3f nbl = nearP._point - up * heightNear/2 - right * widthNear/2;
+	neko::Vec3f ftr = farP._point + up * heightFar/2 + right * widthFar/2;
+	neko::Vec3f ftl = farP._point + up * heightFar / 2 - right * widthFar / 2;
+	neko::Vec3f fbl = farP._point - up * heightFar / 2 - right * widthFar / 2; //Far Bottom Left
+
+	Plane rightP = Plane(ntr, nbr, ftr);
+	Plane leftP = Plane(nbl, ftl, fbl);
+	Plane topP = Plane(ntr, ftr, ftl);
+	Plane bottomP = Plane(nbr, nbl, fbl);
+
+	m_Planes[0] = nearP;
+	m_Planes[1] = farP;
+	m_Planes[2] = rightP;
+	m_Planes[3] = leftP;
+	m_Planes[5] = topP;
+	m_Planes[6] = rightP;
 }
 
 bool Frustum::ContainsPoint(neko::Vec3f point)
